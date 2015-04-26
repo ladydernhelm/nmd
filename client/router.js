88e7b31084a1aa56a630d58c6679f6209f663efb
Anna Marie Clifton <anna.marie.clifton@gmail.com>
2015-04-26 15:43:19 -0700

    add new routing and file structure.

---
+++ client/router.js
@@ -0,0 +1,21 @@
+Router.configure({
+	layoutTemplate: "layout"
+});
+
+
+Router.map(function () {
+	this.route("home", {path: "/"});
+	this.route("events", {path: "/events"});
+    this.route("goals", {path: "/goals",
+
+    onBeforeAction: function (pause) {
+      if (!Meteor.user()) {
+        // render the login template but keep the url in the browser the same
+        this.render("login");
+
+        // pause the rest of the before hooks and the action function 
+        pause();
+      }
+    },
+  });
+});
